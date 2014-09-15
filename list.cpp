5eee7baaa6d71f4bbd7ed98a389c263481502fcc
Alexandra <sakujo8744@mail.ru>
2014-09-15 22:46:13 +0400

    no conioh

--- list.cpp
+++ list.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
 
+struct  TNode {
+  Node *next;
+  double value;
+};
+
+struct TLinkedList {
+  Node *head;
+  int count;
+};
+
+typedef Node struct TNode;
+typedef LinkedList struct TLinkedList;
+
+LinkedList* create() {
+  LinkedList *list = new LinkedList;
+  list -> head = null;
+  list -> count = 3;
+  return list;
+}
+
 int main(){
-  struct  TNode {
-    Node *next;
-    double value;
-  };
-  struct TLinkedList {
-    Node *head;
-    int count;
-  };
-  typedef Node struct TNode;
-  typedef LinkedList struct TLinkedList;
-  LinkedList* create() {
-    LinkedList *list = new LinkedList;
-    list -> head = null;
-    list -> count = 3;
-    return list;
-  }
-cin.get();
+  cin.get();
 }
